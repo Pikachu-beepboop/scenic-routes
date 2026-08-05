741b97b44736ce6407d6b0d8f936e3d545227b1a
Lavr <anichinlavr@gmail.com>
2026-08-05 20:49:48 +0200

    changed for the cloudfare pages to work and build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Wichtig: Statische Next.js Seiten unterstützen standardmäßig keine automatische Bildoptimierung
+  },
+};
 
 export default nextConfig;
